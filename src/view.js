0f730d0e79c0019e61aac399e5ab89a3c6a55a81
Moses Holmström <thykka@gmail.com>
2020-09-05 16:27:17 +0300

    Draw input actions, improve HUD

--- src/view.js
+++ src/view.js
@@ -1,5 +1,8 @@
+import InputActions from './input-actions.js';
+
 const CurrencyUnit = 'pc';
-const UpdateRate = 1000 / 1//2;
+const UpdateRate = 1000 / 8//2;
+const { floor } = Math;
 
 class ConsoleView {
     constructor() {
@@ -22,20 +25,42 @@ class ConsoleView {
     draw(state) {
         console.clear();
         console.log(`${ this.formatStatus(state) }`);
+        console.log(`       Actions: 
+${ this.formatActions(state) }`);
     }
 
     formatStatus(state) {
         return `SimPePu
 
-       Time: ${ state.gameTime.toString() }
-      Money: ${ state.player.money }${ CurrencyUnit }
-    Tickets: ${ state.lottery.tickets.player }
-        Pot: ${ state.lottery.pot }
+          Time: ${ state.gameTime.toString() }
+
+   Last result: ${ this.formatLastResult(state) }
+     Next PePu: ${ state.lottery.time.toString() }
+           Pot: ${ state.lottery.pot }
+Winning chance: ${ floor(state.lottery.tickets.player / state.lottery.pot * 100) || 0 }%
+
+    Your money: ${ state.player.money }${ CurrencyUnit }
+  Your tickets: ${ state.lottery.tickets.player }
 `;
+    }
+
+    formatActions(state) {
+        return Object.entries(InputActions).filter(
+            ([key, action]) => action.allowed(state)
+        ).map(
+            ([key, action]) => `                ${ key } - ${ action.title}`
+        ).join('\n')
     }
 
     currentTime() {
         return Date.now();
+    }
+
+    formatLastResult(state) {
+        if(!state.lastResult) return 'n/a';
+        return (
+            state.lastResult.result ? 'You' : 'Someone else'
+        ) + ' won ' + state.lastResult.pot + CurrencyUnit + ' worth of booze on the last round' + (state.paused ? '!!!' : '');
     }
 }
 
