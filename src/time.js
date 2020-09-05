23badf000e89fa136a3f32933b44fdf146f0aa0d
Moses Holmström <thykka@gmail.com>
2020-09-05 16:27:31 +0300

    Fix time initialization

--- src/time.js
+++ src/time.js
@@ -4,30 +4,33 @@ const HoursPerDay = 24;
 const DayNames = 'Monday Tuesday Wednesday Thursday PePuday Saturday Sunday'.split(' ');
 
 class Time {
-    constructor(time = 0) {
-        this.time = time;
-    }
-
-    get hours() {
-        return floor(this.time) % HoursPerDay;
-    }
-
-    set hours(newHours) {
-        return this.time += newHours - this.hours;
+    constructor(time) {
+        if(typeof time === 'number') {
+            this.time = time;
+        } else if (typeof time === 'object' && time.time) {
+            this.time = time.time;
+        } else {
+            this.time = 0;
+        }
     }
 
     get minutes() {
         return floor((this.time % 1) * MinutesPerHour) % MinutesPerHour;
     }
-
     set minutes(newMinutes) {
         return this.time += (newMinutes - this.minutes) / MinutesPerHour;
+    }
+
+    get hours() {
+        return floor(this.time) % HoursPerDay;
+    }
+    set hours(newHours) {
+        return this.time += newHours - this.hours;
     }
 
     get day() {
         return floor(this.time / HoursPerDay);
     }
-
     set day(newDay) {
         return this.time += (newDay - this.day) * HoursPerDay;
     }
@@ -35,7 +38,6 @@ class Time {
     get week() {
         return floor(this.day / DayNames.length) + 1
     }
-
     set week(newWeek) {
         return this.time += (newWeek - this.week) * HoursPerDay * DayNames.length
     }
@@ -43,7 +45,6 @@ class Time {
     get weekday() {
         return DayNames[this.day % DayNames.length];
     }
-
     set weekday(newWeekday) {
         throw Error(`Cannot set weekday to ${ newWeekday }; weekday is read-only.`);
     }
