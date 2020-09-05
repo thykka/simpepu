f63efb6a7198551916a63ce697b4de8826364f70
Moses Holmström <thykka@gmail.com>
2020-09-05 14:34:48 +0300

    Basic keyb input handling

--- src/input.js
+++ src/input.js
@@ -1,7 +1,32 @@
-class Input {
-    constructor() {
+import InputActions from './input-actions.js';
 
-    }
+const { stdin } = process;
+
+class Input {
+  constructor(state) {
+    this.pressed = {};
+    this.init(state);
+  }
+
+  init(state) {
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding('utf8');
+    stdin.on('data', (key) => {
+      // Ctrl-C
+      if(key === '\u0003') this.quit();
+      const action = InputActions[key];
+      if(!action) return;
+      if(action.allowed(state)) {
+        action.action(state);
+      }
+    });
+  }
+
+  quit() {
+    console.clear();
+    process.exit();
+  }
 }
 
 export default Input;
